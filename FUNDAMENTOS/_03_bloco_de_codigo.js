a41f2dc221cd7878d76f3498a73f2aacff082ab0
Fabiano <fabiano.alves.dev@gmail.com>
2026-01-18 19:04:11 -0300

    Explicando sobre o que são bloco de código

---
+++ FUNDAMENTOS/_03_bloco_de_codigo.js
@@ -0,0 +1,42 @@
+
+
+// Aqui temos uma sentença de código
+console.log("Essa linha toda incluindo o ponto e virgula é uma sentença de código.");
+
+/**
+ * Blocos de código é a junção de várias setenças de 
+ * código juntas dentro de um par de chaves.
+ * Exemplo - Abaixo
+ */
+{
+   console.log("Primeira istrução lida pelo computador.");  ''
+   console.log("Segunda istrução lida pelo computador.");
+   console.log("Terceira istrução lida pelo computador.");
+}
+
+//Algumas estruturas em javaScript apresentam bloco de código como:
+if(true){// Inicio do bloco
+ 
+}// Fim do bloco
+
+
+
+for(let i = 0;i <= 10;i++){// Inicio do bloco
+
+}// Fim do bloco
+
+
+switch(true){// Inicio do bloco
+    case 1:
+        console.log("Teste 1");
+        break;
+    case 2:
+        console.log("Teste 1");
+        break;
+    default:
+        console.log("Default ....");
+}// Fim do bloco
+
+function exemplo(parametro){// Inicio do bloco
+    return parametro;
+}// Fim do bloco
