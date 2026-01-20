e6ec622b52d09951afa560162f82f618ade05751
Fabiano <fabiano.alves.dev@gmail.com>
2026-01-19 22:09:15 -0300

    Sobre tipo de dados

---
+++ FUNDAMENTOS/_06_sobre_dados3.js
@@ -0,0 +1,15 @@
+// Tipos de dados 
+let idade = 40; // number
+
+console.log(typeof 40);
+console.log(typeof idade);
+
+let salario = 45678.32; //number
+console.log(typeof salario);
+
+let estaChovendo = true; // Tipo boolean
+console.log(typeof estaChovendo);
+
+// Tipo string
+console.log(typeof "teste");
+console.log(typeof 'teste');
