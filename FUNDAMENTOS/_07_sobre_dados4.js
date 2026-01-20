f63470b65e2b1d95d9d51efdf7b6bcf19e53ed73
Fabiano <fabiano.alves.dev@gmail.com>
2026-01-19 22:34:26 -0300

    Mais sobre dados

---
+++ FUNDAMENTOS/_07_sobre_dados4.js
@@ -0,0 +1,12 @@
+// Let é uma variavel que pode ser modificada.
+let a = 3;
+a = a + 10;
+console.log(a);
+
+// const é um tipo de variavel que não aceita ser modificada
+const b = 12;
+
+// Vai gerar um erro pois não se pode atribuir um novo valor a uma constante
+b = b + 12; 
+
+console.log(b);
