e216049aa3dc66f9043b8972e37e14fd4e925873
Fabiano <fabiano.alves.dev@gmail.com>
2026-01-30 23:47:30 -0300

    Sobre o operador Destruturing

--- _02_FUNDAMENTOS_JAVASCRIPT/_21_Operador_Destructuring.js
+++ _02_FUNDAMENTOS_JAVASCRIPT/_21_Operador_Destructuring.js
@@ -5,7 +5,7 @@ const pessoa = {
     nome:"Tatiana",
     idade:47,
     endereco:{
-        rualogradouro:"Rua Ç",
+        logradouro:"Rua Ç",
         bairro:"Açude"
     }
 }
@@ -16,6 +16,18 @@ console.log(idade);
 
 console.log();
 
+// Caso você precise pode criar variáveis com outro nome para receber esses valores do objeto.
 const {nome:i,idade:h} = pessoa;
 console.log(i);
 console.log(h);
+
+// Retirar o valor de dentro do objeto,que está dentro desse objeto
+const {endereco:{logradouro,bairro}} = pessoa;
+console.log("Rua: ",logradouro);
+console.log("Bairro: ",bairro);
+
+// Caso você tente acessar alguma variável que não existe no objeto 
+// ou gera um erro, ou retorna um andefined
+const {email,telefone} = pessoa;
+console.log(email);
+console.log(telefone);
