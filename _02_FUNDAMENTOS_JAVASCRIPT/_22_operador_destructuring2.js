f1a1bcd48d8d0809b6fef628f269903b8f60d75e
Fabiano <fabiano.alves.dev@gmail.com>
2026-02-01 17:01:47 -0300

    Mais sobre operador destructuring

---
+++ _02_FUNDAMENTOS_JAVASCRIPT/_22_operador_destructuring2.js
@@ -0,0 +1,12 @@
+// Exemplo 1
+const [a] = [10];
+console.log(a);
+
+// Exemplo 2
+const [j, b, c, d, e, f] = [1,"f",3,4,"teste",10];
+console.log("A: " + j);
+console.log("B: " + b);
+console.log("C: " + c);
+console.log("D: " + d);
+console.log("E: " + e);
+console.log("F: " + f);
---
+++ _02_FUNDAMENTOS_JAVASCRIPT/_23_operador_destructuring3.js
@@ -0,0 +1,12 @@
+function rand({min = 0,max = 1000}){
+    const valor = Math.random() * (max - min) + min;
+    return Math.floor(valor);
+}
+
+const obj = {min: 40,max:40};
+console.log(rand(obj));
+
+console.log(rand({min:955}));
+
+console.log(rand({}));
+
